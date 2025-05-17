3ef0fe9e576803b9d2d24997513072202bf5a141
Todor Bonev <totbon77@gmail.com>
2025-05-17 12:55:11 +0300

    Initial commit

---
+++ JavaScript/easy/AddBinary.js
@@ -0,0 +1,12 @@
+function addBinary(a, b) { 
+
+    let num1 = BigInt("0b" + a); 
+    let num2 = BigInt("0b" + b); 
+
+    let sum = num1 + num2;
+
+
+    return sum.toString(2);
+}
+
+console.log(addBinary("11", "1"));
